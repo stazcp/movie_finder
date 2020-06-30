082e994798b276763cb6e64187ce17c01c807ea6
Staz <37573648+stazcp@users.noreply.github.com>
2020-06-30 11:39:00 -0700

    Final v1 needs some touch-ups but, it's good enough for now.

--- src/index.js
+++ src/index.js
@@ -4,18 +4,53 @@ const APIKEY = "6d46d44f7f8f21704305048480d2f128";
 const baseURL = "https://api.themoviedb.org/3/";
 let poster_size = "w300";
 let baseImageURL = null;
-let searchForm = document.getElementById("search");
-let infoModal = document.getElementById("infoModal");
-let closeModal = infoModal.querySelector("button");
 let page = 1;
-let currentPage = 1;
 let totalPages = 0;
 let searchID = "popular";
 let searchValue = null;
+let searchForm = document.getElementById("search");
+let infoModal = document.getElementById("infoModal");
+let closeModal = infoModal.querySelector("button");
 
+//event.preventDefault
+//init is called at the bottom
 const init = () => {
-  getConfig();
-  setPage();
+  console.log("initializing");
+  console.log(document.querySelector("output"));
+  if (document.querySelector("output") === null) {
+    getConfig();
+    setPage();
+  }
+  //event listeners
+  document.getElementById("nextPageBtn").addEventListener("click", e => {
+    getNextPage();
+  });
+  document.getElementById("prevPageBtn").addEventListener("click", e => {
+    getPrevPage();
+  });
+  // request a specific page, results don't persist!
+  document.getElementById("goToPage").addEventListener("click", e => {
+    e.preventDefault();
+    page = document.getElementById("getPageNo").selectPage.value;
+    getMovies();
+  });
+  // the id of dropdown ele will be used to retrieve relative movies
+  document.getElementById("categories").addEventListener("click", e => {
+    searchID = e.target.id;
+    searchValue = null;
+    page = 1;
+    // setPage();
+    getMovies();
+  });
+  //when a movie is searched for
+  searchForm.addEventListener("submit", event => {
+    event.preventDefault(); //prevents page from refreshing after getting results
+    searchValue = searchForm.search.value;
+    page = 1;
+    setPage();
+    document.querySelector("h4").innerText = "";
+    runSearch(searchValue);
+  });
 };
 
 const getConfig = () => {
@@ -41,55 +76,38 @@ const capitalize = string => {
   // return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
+//display's page numbers and category
 const setPage = () => {
-  document.getElementById("currentPage").innerText = currentPage;
+  document.getElementById("currentPage").innerText = page;
   document.getElementById("nOfPages").innerText = totalPages;
   document.querySelector("h4").innerText = capitalize(searchID);
 };
 
 const getNextPage = () => {
   page++;
+  setPage();
   if (searchValue === null) {
     getMovies();
   } else {
     runSearch(searchValue);
   }
-  currentPage = page;
-  setPage();
 };
 
 const getPrevPage = () => {
-  if (currentPage > 1) {
-    page--;
+  page--;
+  setPage();
+  if (page > 0) {
     if (searchValue === null) {
       getMovies();
     } else {
       runSearch(searchValue);
     }
-    currentPage = page;
-    setPage();
   }
 };
 
-document.getElementById("nextPageBtn").addEventListener("click", e => {
-  getNextPage();
-});
-document.getElementById("prevPageBtn").addEventListener("click", e => {
-  getPrevPage();
-});
-
-// the id of dropdown will be the id for the url to retrieve relative movies
-document.getElementById("categories").addEventListener("click", e => {
-  searchID = e.target.id;
-  searchValue = null;
-  page = 1;
-  currentPage = page;
-  setPage();
-  getMovies();
-});
-
 const getMovies = () => {
   let movieArr = [];
+  console.log(`get page: ${page}`);
   let url = `${baseURL}movie/${searchID}?api_key=${APIKEY}&language=en-US&page=${page}`;
   fetch(url)
     .then(result => result.json())
@@ -112,23 +130,11 @@ const runSearch = keyword => {
       //process the returned data
       movieArr.push(...data.results);
       totalPages = data.total_pages;
-      console.log(data.total_pages);
       setPage();
       displayPosters(movieArr);
     })
     .catch(err => console.log(err));
 };
-
-//search event listener, triggers a search
-searchForm.addEventListener("submit", event => {
-  event.preventDefault(); //prevents page from refreshing after getting results
-  searchValue = searchForm.search.value;
-  page = 1;
-  currentPage = page;
-  setPage();
-  document.querySelector("h4").innerText = "";
-  runSearch(searchValue);
-});
 
 const onClickMovie = movie => {
   showModal(movie);
@@ -154,9 +160,15 @@ const hideModal = () => {
 };
 
 const displayPosters = data => {
-  if (data.length < 1) return;
+  if (data.length < 1) {
+    document.getElementById("output").innerHTML =
+      "<h1>No movies found, please try again.</h1>";
+    document.querySelector("h4").innerText = "";
+    return null;
+  }
   document.getElementById("output").innerHTML = null;
   data.map(movie => {
+    if (!movie.poster_path) return undefined;
     let poster = `${baseImageURL}${poster_size}${movie.poster_path}`;
     let moviePoster = document.createElement("img");
     moviePoster.src = poster;
@@ -164,7 +176,6 @@ const displayPosters = data => {
     moviePoster.setAttribute("data-toggle", "modal");
     moviePoster.setAttribute("data-target", "infoModal");
     moviePoster.setAttribute("role", "button");
-
     moviePoster.onclick = () => onClickMovie(movie); //wrapping the func to not execute without click
     document.getElementById("output").appendChild(moviePoster);
     return null;
